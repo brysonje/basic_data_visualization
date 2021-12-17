2e15a75e1cb74ee519e2119ad8174b285152284c
Jhonny Espinoza Bryson <bryson_je@hotmail.com>
2021-12-17 10:00:37 -0300

    Update data_analysis_app.py

--- data_analysis_app.py
+++ data_analysis_app.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # initial settings
 ready_analysis = False
 
-# st.sidebar.header("Here is a sidebar")
+# title
 st.title("Basic Data Visualization")
 col1, col2 = st.columns(2)
 with col1:
@@ -17,16 +17,20 @@ with col2:
     st.write("**Strength Prediction** [link](https://strengthprediction.herokuapp.com)")
 link_openMV = "https://openmv.net"
 st.write("We will use a file from **OpenMV.net**: distillation-tower.csv  [link](https://openmv.net)")
+st.write("Please, use the left side panel to input your selections")
 st.write("1. Let's start by **uploading** the file")
 
-# upload the csv file
-st.sidebar.header("Use this panel for user input")
+# sidebar
+st.sidebar.header("Left side panel for user input")
+
+# uploading the csv file
 upload_csv_file = st.sidebar.checkbox("Click here to upload the csv file")
 if upload_csv_file:
     df = pd.read_csv("https://openmv.net/file/distillation-tower.csv")
     st.write("File has:", df.shape[1], "columns and:", df.shape[0], "rows")
     st.dataframe(data = df, width = 700, height = 150)
     st.write("2. Select the **amount** and the **variables** and for **analysis**")
+    st.write("Check the **left box** when ready")
     df["time"] = np.arange(0, df.shape[0])
     amount_variables = ["one", "two", "tree", "four"]
     amount_variable = st.sidebar.radio("Select how much variables to analyze:", amount_variables)
@@ -45,8 +49,8 @@ if upload_csv_file:
 
 # Showing plots
 if ready_analysis:
-    st.write("3. Here below is the **visualization**")
     if amount_variable == "one" and len(columns) == 1:
+        st.write("3. Here below are tree graph for **visualization**:", columns)
         col1, col2 = st.columns(2)
         fig1, ax1 = plt.subplots(figsize = (6, 2.5))
         ax1.hist(df[columns], bins = 40)
@@ -63,11 +67,13 @@ if ready_analysis:
         sns.lineplot(data = data, x = "time", y = columns[0])
         st.pyplot(fig3)
     if amount_variable == "two" and len(columns) == 2:
+        st.write("3. Here below is a **scatter plot** for:", columns[0], columns[1])
         fig, ax = plt.subplots(figsize = (3, 3))
         fig = sns.jointplot(x = df[columns[0]], y = df[columns[1]],
             kind = "scatter", height = 10, marginal_kws = dict(bins = 25, fill = True))
         st.pyplot(fig)
     if amount_variable == "tree" and len(columns) == 3:
+        st.write("3. Here below are a couple of **plots** for:", columns[0], columns[1], columns[2])
         data = df[[columns[0], columns[1], columns[2]]]
         chart = alt.Chart(data).mark_circle(size = 20).encode(x = columns[0], y = columns[1],
         color = columns[2], tooltip = [columns[0], columns[1], columns[2]]).interactive()
@@ -76,6 +82,7 @@ if ready_analysis:
         fig = sns.pairplot(data, height = 4)
         st.pyplot(fig)
     if amount_variable == "four" and len(columns) == 4:
+        st.write("3. Here below a **correlation heatmap** and basic data **information** for:", columns[0], columns[1], columns[2], columns[3])
         data = df[[columns[0], columns[1], columns[2], columns[3]]]
         fig, ax = plt.subplots(figsize = (5, 5))
         sns.heatmap(data.corr(), ax = ax, cmap = "YlGnBu", linewidths = 0.1)
@@ -86,3 +93,4 @@ if ready_analysis:
         with col2:
             st.write("**basic** data information")
             st.dataframe(data = data.describe())
+            st.write("Please, **uncheck** the last box before change your options")
